b68d0fd22f6f09ca148791816439b4c8f08bb8e2
Programmering <teknisk+programmering-GDC2025-GR7@game.unf.dk>
2025-07-11 14:28:40 +0200

    del 1 færdig

---
+++ Assets/PlayerScript.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class PlayerScript : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        print("Hej fra start");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        print("Hej fra update");
+    }
+}
